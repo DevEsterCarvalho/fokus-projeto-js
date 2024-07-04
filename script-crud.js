300a6e9a4f09089badcc4bf3e152e703d7155f68
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-07-04 15:17:48 -0300

    subindo edicoes

--- script-crud.js
+++ script-crud.js
@@ -1,1 +1,7 @@
-document.querySelector('')
+const btnAdicionarTarefa = document.querySelector('app__button--add-task')
+const formAdicionarTarefa = document.querySelector('app__form-add-task')
+
+btnNovaTarefa.addEventListener('click', () =>{
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
