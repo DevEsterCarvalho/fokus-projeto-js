c04a5cbc885d3c3253c3bb5204e8aa777c5ce55b
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-07-10 19:54:47 -0300

    editando botoes do formulario

--- script-crud.js
+++ script-crud.js
@@ -2,8 +2,14 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
+const botaoCancelar = document.querySelector('.app__form-footer__button--cancel')
+const formularioTarefa = formAdicionarTarefa
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+function atualizarTarefas() {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}  
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
@@ -22,6 +28,21 @@ function criarElementoTarefa(tarefa) {
 
     const botao = document.createElement('button')
     botao.classList.add('app_button-edit')
+
+    botao.onclick = () => {
+        //debugger
+        const novaDescricao = prompt("Qual é o novo nome da tarefa?")
+        //console.log('Nova descrição da tarefa: ', novaDescricao)
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao
+        tarefa.descricao = novaDescricao
+        atualizarTarefas()
+        }
+    }
+    botaoCancelar.addEventListener('click', () => {
+        paragrafo.textContent = ' '
+        formularioTarefa.classList.add('hidden')
+    })
 
     const imagemBotao = document.createElement('img')
     imagemBotao.setAttribute('src', '/imagens/edit.png')
@@ -46,7 +67,7 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa)
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    atualizarTarefas()
     textarea.value = ''
     formAdicionarTarefa.classList.add('hidden')
 })
